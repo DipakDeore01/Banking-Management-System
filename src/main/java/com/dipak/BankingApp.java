9c32b110ce73523021e6fb6f73c30e5c962b4490
Dipak Deore <dipaksdeore2004@gmail.com>
2025-09-27 00:55:06 +0530

    Created Main Menu

--- src/main/java/com/dipak/BankingApp.java
+++ src/main/java/com/dipak/BankingApp.java
@@ -1,7 +1,32 @@
 package com.dipak;
 
+import java.util.Scanner;
+
 public class BankingApp {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("+---------------------------------+\n" +
+                    "|    Banking Management System    |\n" +
+                    "+---------------------------------+");
+            System.out.println("1) Register \n" +
+                    "2) Login \n" +
+                    "0) Exit \n");
+            System.out.print("Enter Your Choice: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1 -> System.out.println("Register...");
+                case 2 -> System.out.println("Login...");
+                case 0 -> {
+                    System.out.println("Exiting Software...");
+                    return;
+                }
+                default -> System.out.println("Enter Valid Choice...\n");
+            }
+        }
+
     }
 }
