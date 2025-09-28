7378b498ee1d781e3ce4e7988cb2ee5031158f33
Dipak Deore <dipaksdeore2004@gmail.com>
2025-09-29 00:58:46 +0530

    Creates JDBC connection in main class and new class added User.java, Created login and register methods

--- src/main/java/com/dipak/BankingApp.java
+++ src/main/java/com/dipak/BankingApp.java
@@ -1,32 +1,49 @@
 package com.dipak;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class BankingApp {
+    private static final String url = "jdbc:mysql://localhost:3306/bankingapp";
+    private static final String username = "root";
+    private static final String password = "2004";
+
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Scanner sc = new Scanner(System.in);
+            User user = new User(connection, sc);
 
-        while (true) {
-            System.out.println("+---------------------------------+\n" +
-                    "|    Banking Management System    |\n" +
-                    "+---------------------------------+");
-            System.out.println("1) Register \n" +
-                    "2) Login \n" +
-                    "0) Exit \n");
-            System.out.print("Enter Your Choice: ");
-            int choice = sc.nextInt();
+            String email;
+            long account_number;
 
-            switch (choice) {
-                case 1 -> System.out.println("Register...");
-                case 2 -> System.out.println("Login...");
-                case 0 -> {
-                    System.out.println("Exiting Software...");
-                    return;
+            while (true) {
+                System.out.println("+---------------------------------+\n" +
+                        "|    Banking Management System    |\n" +
+                        "+---------------------------------+");
+                System.out.println("1) Register \n" +
+                        "2) Login \n" +
+                        "0) Exit \n");
+                System.out.print("Enter Your Choice: ");
+                int choice = sc.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        user.register();
+                    case 2:
+                        email = user.login();
+                    case 0:
+                        System.out.println("Exiting Software...");
+                        return;
+                    default:
+                        System.out.println("Enter Valid Choice...\n");
                 }
-                default -> System.out.println("Enter Valid Choice...\n");
             }
+        }catch(SQLException e) {
+            e.printStackTrace();
         }
-
     }
 }
