b2e9a341d49e31ecc91ecc2c0cf20f601ca9c554
Dipak Deore <dipaksdeore2004@gmail.com>
2025-09-29 23:59:00 +0530

    Added Account.java class and updated BankingApp.java

--- src/main/java/com/dipak/BankingApp.java
+++ src/main/java/com/dipak/BankingApp.java
@@ -16,6 +16,7 @@ public class BankingApp {
             Connection connection = DriverManager.getConnection(url, username, password);
             Scanner sc = new Scanner(System.in);
             User user = new User(connection, sc);
+            Accounts accounts = new Accounts(connection, sc);
 
             String email;
             long account_number;
@@ -35,11 +36,46 @@ public class BankingApp {
                         user.register();
                     case 2:
                         email = user.login();
-                    case 0:
-                        System.out.println("Exiting Software...");
+                        if(email!=null){
+                            System.out.println();
+                            System.out.println("User Logged In!");
+                            if(!accounts.account_exist(email)){
+                                System.out.println();
+                                System.out.println("1. Open a new Bank Account");
+                                System.out.println("2. Exit");
+                                if(sc.nextInt() == 1) {
+                                    account_number = accounts.open_account(email);
+                                    System.out.println("Account Created Successfully");
+                                    System.out.println("Your Account Number is: " + account_number);
+                                }else{
+                                    break;
+                                }
+
+                            }
+                            account_number = accounts.getAccount_number(email);
+                            int choice2 = 0;
+                            while (choice2 != 5) {
+                                System.out.println();
+                                System.out.println("1. Debit Money");
+                                System.out.println("2. Credit Money");
+                                System.out.println("3. Transfer Money");
+                                System.out.println("4. Check Balance");
+                                System.out.println("5. Log Out");
+                                System.out.println("Enter your choice: ");
+                                choice2 = sc.nextInt();
+                            }
+
+                        }
+                        else{
+                            System.out.println("Incorrect Email or Password!");
+                        }
+                    case 3:
+                        System.out.println("THANK YOU FOR USING BANKING SYSTEM!!!");
+                        System.out.println("Exiting System!");
                         return;
                     default:
-                        System.out.println("Enter Valid Choice...\n");
+                        System.out.println("Enter Valid Choice");
+                        break;
                 }
             }
         }catch(SQLException e) {
